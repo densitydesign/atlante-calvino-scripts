e357373ba30e6c440407e2009cdb187f0378613d
iosonosempreio <tommaso.elli@gmail.com>
2018-04-26 10:06:31 +0200

    HOLA

--- saggi/parse-pages-references.js
+++ saggi/parse-pages-references.js
@@ -19,7 +19,7 @@ node_xj({
                 'name': d.name,
                 'references': []
             }
-            console.log(d.name);
+            console.log('\x1b[30m', d.name);
 
 
             d.references = d.references.split(',');
@@ -33,35 +33,98 @@ node_xj({
 
                 // // if the reference is a page number, return it as it is
                 if (!isNaN(r)) {
-                    console.log(`${r} is a number`)
+                    console.log('\x1b[30m', `${r} is a number`)
                     obj = {
                         'page': parseInt(r),
                         'intoNote': false,
                         'noteNumber': null
                     }
-
+                    person.references.push(obj);
                 } else if (r.indexOf('-') > -1 && r.indexOf('n') < 0) {
-                		console.log(`${r} are multiple pages`)
+                    console.log('\x1b[30m', `${r} are consequential pages`)
 
-                		r = r.split('-');
+                    // E.g. "1352-64" > grab 64 and 52, and generate all numbers from 1352 to 1364
+                    r = r.split('-');
+                    let lastDigits = r[0].toString().substr(r[0].toString().length - r[1].toString().length);
+                    let difference = r[1] - lastDigits;
 
-                		console.log(r, r[1].toString().length, r[0].toString().length);
-
-                		let lastDigits = r[0].toString().substr(r[0].toString().length - r[1].toString().length);
-
-                		console.log(r, r[1], lastDigits)
-
-
+                    // Push to array first number, then calculate the rest of the sequence
                     obj = {
-                        'page': 'nan',
+                        'page': parseInt(r[0]),
                         'intoNote': false,
                         'noteNumber': null
                     }
+                    person.references.push(obj);
+
+                    // The rest of the sequence
+                    for (var i = 1; i <= difference; i++) {
+                        r[0]++;
+                        console.log(r[0])
+                        obj = {
+                            'page': r[0],
+                            'intoNote': false,
+                            'noteNumber': null
+                        }
+                        person.references.push(obj);
+                    }
+                } else if (r.indexOf('n') > -1 && r.indexOf('-') < 0) {
+                    console.log('\x1b[30m', `${r} is in note`);
+                    obj = {
+                        'page': parseInt(r.split(' n')[0]),
+                        'intoNote': true,
+                        'noteNumber': null
+                    }
+                    person.references.push(obj);
+                } else if (r.indexOf('n') > -1 && r.indexOf('-') > -1) {
+                    if (r.indexOf('n') > r.indexOf('-')) {
+                        console.log('\x1b[31m', `COMPLEX: ${r} is on multiple pages, may be singular note`);
+                        console.log('\x1b[30m', r.split(' n')[0])
+
+                        // E.g. "1352-64" > grab 64 and 52, and generate all numbers from 1352 to 1364
+                        r = r.split(' n')[0];
+                        r = r.split('-');
+                        let lastDigits = r[0].toString().substr(r[0].toString().length - r[1].toString().length);
+                        let difference = r[1] - lastDigits;
+
+                        // Push to array first number, then calculate the rest of the sequence
+                        obj = {
+                            'page': parseInt(r[0]),
+                            'intoNote': false,
+                            'noteNumber': null
+                        }
+                        person.references.push(obj);
+
+                        // The rest of the sequence
+                        for (var i = 1; i <= difference; i++) {
+                            r[0]++;
+                            console.log(r[0])
+                            obj = {
+                                'page': r[0],
+                                'intoNote': true,
+                                'noteNumber': null
+                            }
+                            person.references.push(obj);
+                        }
+
+                    } else {
+                        console.log('\x1b[31m', `COMPLEX: ${r} is on singular page, may be more notes`);
+
+                        r = r.split(' n')[0];
+                        console.log(r)
+                        obj = {
+                            'page': parseInt(r),
+                            'intoNote': true,
+                            'noteNumber': null
+                        }
+                        person.references.push(obj);
+                    }
+
+
+
                 } else {
-                	console.log(`${r} ???????????`)
+                    console.log('\x1b[30m', `${r} ???????????`)
                 }
 
-                person.references.push(obj);
 
                 // if (r.indexOf('-') > -1) {
                 // 	r = r.split('-');
@@ -75,14 +138,15 @@ node_xj({
 
             data.push(person);
 
-            console.log('')
+            console.log('\x1b[30m', '')
         });
         // console.log(data);
         fs.writeFile("data/references-names.json", JSON.stringify(data, null, 2), function(err) {
             if (err) {
                 return console.log(err);
             }
-            console.log("The file was saved!");
+            console.log('\x1b[32m', "The file was saved!");
+            console.log('\x1b[30m')
         });
     }
 });
